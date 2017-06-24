cc71ca01d969ae628745aefe44235e7dec4ba696
Jeremy Friesner <jaf@meyersound.com>
2017-06-23 17:44:25 -0700

    Separated source from MacOS install

--- demos/choir/package_for_macosx.sh
+++ demos/choir/package_for_macosx.sh
@@ -25,30 +25,12 @@ pushd "${SCRIPT_PATH}"
   mv "./${CHOIR_APP_NAME}.app" "${ZGCHOIR_OUTPUT_PATH}"
   # TODO:  codesign the .app here!
 
-  # Place a clean checkout of the current zg_choir git repository into the output folder as well
-  pushd "${SCRIPT_PATH}/../.."
-  git archive --format zip --output "${ZGCHOIR_OUTPUT_PATH}/zg_choir.zip" master
-  popd   # pop back to ${SCRIPT_PATH}
-
-  # Place the zg_choir and muscle source archives into the "zg_choir" subfolder of the distribution, for convenience
   pushd "${ZGCHOIR_FOR_MACOSX_DIR_PATH}"
     pushd "${ZGCHOIR_OUTPUT_PATH}"
-      mkdir zg_choir
-      pushd zg_choir
-        unzip -q ../zg_choir.zip ; rm ../zg_choir.zip
-        pushd html/autodocs
-           chmod ugo+rx ./genDocs.sh
-           ./genDocs.sh
-        popd
-        svn export https://github.com/jfriesne/muscle/trunk
-        mv trunk muscle
-      popd   # pop back to ${ZGCHOIR_OUTPUT_PATH}
-
       README_NAME="README.html"
-      cp "zg_choir/demos/choir/html/${README_NAME}" .
-      sed -i .bak 's/images\/ZGChoir/zg_choir\/demos\/choir\/html\/images\/ZGChoir/g' "./${README_NAME}"  # fix relative links since we moved the HTML
-      rm -f "${README_NAME}".bak
-      ln -s zg_choir/demos/choir/songs/ .
+      cp "${SCRIPT_PATH}/html/${README_NAME}" .
+      cp -R "${SCRIPT_PATH}/html/images" ./images
+      cp -R "${SCRIPT_PATH}/songs" ./songs
     popd   # pop back to the ${ZGCHOIR_FOR_MACOSX_DIR_PATH}
     ln -s /Applications "Drag here to install"
   popd   # pop back to the ${SCRIPT_PATH}
