efdfce5c43a3dc03595b9391d8f27ad898ebb3e7
Ruurd Adema <ruurdadema@gmail.com>
2018-05-22 12:00:01 +0200

    Updated documentation

--- include/zg/ZGPeerSession.h
+++ include/zg/ZGPeerSession.h
@@ -167,8 +167,13 @@ protected:
    /** Implemented as a no-op, since by default this session doesn't have a gateway */
    virtual void MessageReceivedFromGateway(const MessageRef & msg, void * userData);
 
-   /** Implemented as a no-op so we don't force subclasses to implement this method */
-   virtual void MessageReceivedFromPeer(const ZGPeerID & peerID, const MessageRef & msg);
+   /** This method will be called when a message is sent to us by a peersession.
+     * A subclass should override this message to catch all the arbitray messages as this function is implemented as no-op
+     * and will print a warning about receiving an unknown message.
+     * @param fromPpeerID The peerID of the sender
+     * @param msg The message sent to this session
+     */
+   virtual void MessageReceivedFromPeer(const ZGPeerID & fromPeerID, const MessageRef & msg);
 
    /** Must be implemented to reset local state of the specified database to its well-known default state.
      * @param whichDatabase The index of the database to reset (e.g. 0 for the first database, 1 for the second, and so on)
