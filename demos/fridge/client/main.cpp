6b667340054338a9fcb2a1a22f7d6242023ceddf
Jeremy Friesner <jaf@meyersound.com>
2020-05-06 20:09:51 -0700

    And main.cpp too

---
+++ demos/fridge/client/main.cpp
@@ -0,0 +1,22 @@
+#include <QApplication>
+#include "zg/platform/qt/QtCallbackMechanism.h"
+#include "FridgeClientWindow.h"
+
+int main(int argc, char ** argv)
+{
+   using namespace fridge;
+
+   CompleteSetupSystem css;
+   QApplication app(argc, argv);
+
+   // Network I/O threads will rely on this object to call callback
+   // methods safely from within the context of the main/GUI/Qt thread.
+   QtSocketCallbackMechanism qcbm;
+
+   // must be on the heap since we call 
+   // setAttribute(Qt::WA_DeleteOnClose) in the FridgeClientWindow constructor
+   FridgeClientWindow * fsw = new FridgeClientWindow(&qcbm);
+   fsw->show();
+
+   return app.exec();
+}
