a62c1138f6f3d6c730f606c2d0f131fe1d20d31b
Jeremy Friesner <jaf@meyersound.com>
2020-05-15 17:46:25 -0700

    Made the ClientDataMessageTreeDatabaseObject resync more robust

--- src/messagetree/server/ClientDataMessageTreeDatabaseObject.cpp
+++ src/messagetree/server/ClientDataMessageTreeDatabaseObject.cpp
@@ -7,9 +7,11 @@ namespace zg
 
 // Command codes used internally by ClientDataMessageTreeDatabaseObject implementation
 enum {
-   CLIENTDATA_COMMAND_UPLOADLOCALDATA = 1667526004, // 'cdmt' 
+   CLIENTDATA_COMMAND_UPLOADLOCALDATA = 1667526004, // 'cdmt' -- send by senior peer to a junior peer when the senior peer wants the shared-local-data-cache for that junior peer to be refreshed
+   CLIENTDATA_COMMAND_LOCALDATA,                    // reply back from the junior peer to the senior peer
 };
-
+static const String CLIENTDATA_NAME_PATH    = "pth";
+static const String CLIENTDATA_NAME_PAYLOAD = "pay";
 
 ClientDataMessageTreeDatabaseObject :: ClientDataMessageTreeDatabaseObject(MessageTreeDatabasePeerSession * session, int32 dbIndex, const String & rootNodePath) 
    : MessageTreeDatabaseObject(session, dbIndex, rootNodePath)
@@ -156,6 +158,8 @@ void ClientDataMessageTreeDatabaseObject :: MessageReceivedFromMessageTreeDataba
       case CLIENTDATA_COMMAND_UPLOADLOCALDATA:
       {
          LogTime(MUSCLE_LOG_INFO, "ClientDataMessageTreeDatabaseObject:  Re-uploading local client data to the ZG shared database.\n");
+
+         MessageRef replyMsg;
          for (HashtableIterator<const String *, AbstractReflectSessionRef> iter(GetMessageTreeDatabasePeerSession()->GetSessions()); iter.HasData(); iter++)
          {
             ServerSideMessageTreeSession * ssmts = dynamic_cast<ServerSideMessageTreeSession *>(iter.GetValue()());
@@ -167,17 +171,51 @@ void ClientDataMessageTreeDatabaseObject :: MessageReceivedFromMessageTreeDataba
                   status_t ret;
                   if (ssmts->SaveNodeTreeToMessage(*subtreeMsg(), ssmts->GetSessionNode()(), GetEmptyString(), true).IsOK(ret))
                   {
+                     // Note that the reason I'm sending a CLIENTDATA_COMMAND_LOCALDATA Message back to the senior peer's database instead
+                     // of just calling MessageTreeDatabaseObject::UploadNodeSubtree() directly from here is only because I'm paranoid that
+                     // in a chaotic situation where the old senior peer has just gone offline, the junior peers might not yet realize who
+                     // the new senior peer is, and thus a simple call to UploadNodeSubtree() might end up being sent to the old senior peer
+                     // who (being dead) will not act on it.  By sending the Message back explicitly to the (sourcePeer) who sent us the
+                     // CLIENTDATA_COMMAND_UPLOADLOCALDATA Message, I avoid that potential "gotcha".  --jaf
                      const String sharedPath = GetSharedPathFromLocalPathAux(GetEmptyString(), ssmts);
-                     if (MessageTreeDatabaseObject::UploadNodeSubtree(sharedPath, subtreeMsg, TreeGatewayFlags()).IsError(ret)) LogTime(MUSCLE_LOG_ERROR, "ClientDataMessageTreeDatabaseObject:  UploadNodeSubtree(%s) failed for client session [%s] [%s]\n", sharedPath(), ssmts->GetSessionRootPath()(), ret());
-
+                     if (replyMsg() == NULL) replyMsg = GetMessageFromPool(CLIENTDATA_COMMAND_LOCALDATA);
+                     if (replyMsg())
+                     {
+                        const status_t ret = replyMsg()->AddString(CLIENTDATA_NAME_PATH, sharedPath) | replyMsg()->AddMessage(CLIENTDATA_NAME_PAYLOAD, subtreeMsg);
+                        if (ret.IsError()) 
+                        {
+                           LogTime(MUSCLE_LOG_ERROR, "ClientDataMessageTreeDatabaseObject:  Unable to populate CLIENTDATA_COMMAND_LOCALDATA Message!  [%s]\n", ret());
+                           return;
+                        }
+                     }
+                     else WARN_OUT_OF_MEMORY;
                   }
                   else LogTime(MUSCLE_LOG_ERROR, "ClientDataMessageTreeDatabaseObject:  Unable to save client session [%s]'s local data-subtree to a Message! [%s]\n", ssmts->GetSessionRootPath()(), ret());
                }
                else WARN_OUT_OF_MEMORY;
             }
          }
+
+         status_t ret;
+         if ((replyMsg())&&(SendMessageToDatabaseObject(sourcePeer, replyMsg, sourceDBIdx).IsError(ret))) LogTime(MUSCLE_LOG_ERROR, "ClientDataMessageTreeDatabaseObject:  Unable to send CLIENTDATA_COMMAND_LOCALDATA back to source peer!  [%s]\n", ret());
       }
       break;
+
+      case CLIENTDATA_COMMAND_LOCALDATA:
+      {
+         if (GetDatabasePeerSession()->IAmTheSeniorPeer())
+         {
+            const String * nextPath;
+            MessageRef nextPayload;
+            for (int32 i=0; ((msg()->FindString(CLIENTDATA_NAME_PATH, i, &nextPath).IsOK())&&(msg()->FindMessage(CLIENTDATA_NAME_PAYLOAD, i, nextPayload).IsOK())); i++)
+            {
+               status_t ret;
+               if (MessageTreeDatabaseObject::UploadNodeSubtree(*nextPath, nextPayload, TreeGatewayFlags()).IsError(ret)) LogTime(MUSCLE_LOG_ERROR, "ClientDataMessageTreeDatabaseObject:  shared-data UploadNodeSubtree(%s) failed.  [%s]\n", nextPath->Cstr(), ret());
+            }
+         }
+      }
+      break;
+
 
       default:
          MessageTreeDatabaseObject::MessageReceivedFromMessageTreeDatabaseObject(msg, sourcePeer, sourceDBIdx);
