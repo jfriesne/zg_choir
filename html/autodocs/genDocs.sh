578ef1eb6e1e895fc0438a68c5e1cbc97ca15940
Jeremy Friesner <jaf@meyersound.com>
2021-09-02 07:19:10 -0700

    Fixed version-string extraction

--- html/autodocs/genDocs.sh
+++ html/autodocs/genDocs.sh
@@ -3,6 +3,6 @@ echo "Generating doxygen documentation.  "
 echo "You will need to have doxygen installed in"
 echo "order for this script to work."
 echo ""
-export ZG_VERSION=$(grep ZG_VERSION_STRING ../../include/zg/ZGConstants.h | cut -c 26-31)
+export ZG_VERSION=$(grep ZG_VERSION_STRING ../../include/zg/ZGConstants.h | cut -c 28-31)
 doxygen zg.dox
 echo "Done!"
